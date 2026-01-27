557bcdbbdd913ce30c7d94c27d4de3ba121ea578
Steve Gontzes <gontzess@gmail.com>
2026-01-27 15:25:50 -0500

    Upgrade Go to 1.25.2, baton-sdk to v0.7.4, update CI and golangci-lint v2

--- cmd/baton-rippling/main.go
+++ cmd/baton-rippling/main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/conductorone/baton-rippling/pkg/connector"
 	"github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
+	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
 	"github.com/conductorone/baton-sdk/pkg/field"
 	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
@@ -27,6 +28,7 @@ func main() {
 		"baton-rippling",
 		getConnector,
 		cfg.Config,
+		connectorrunner.WithDefaultCapabilitiesConnectorBuilder(&connector.Connector{}),
 	)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
--- pkg/config/conf.gen.go
+++ pkg/config/conf.gen.go
@@ -7,7 +7,7 @@ type Rippling struct {
 	ApiToken string `mapstructure:"api-token"`
 }
 
-func (c* Rippling) findFieldByTag(tagValue string) (any, bool) {
+func (c *Rippling) findFieldByTag(tagValue string) (any, bool) {
 	v := reflect.ValueOf(c).Elem() // Dereference pointer to struct
 	t := v.Type()
 
@@ -39,11 +39,13 @@ func (c *Rippling) GetString(fieldName string) string {
 	if !ok {
 		return ""
 	}
-	t, ok := v.(string)
-	if !ok {
-		panic("wrong type")
+	if t, ok := v.(string); ok {
+		return t
 	}
-	return t
+	if t, ok := v.([]byte); ok {
+		return string(t)
+	}
+	panic("wrong type")
 }
 
 func (c *Rippling) GetInt(fieldName string) int {
