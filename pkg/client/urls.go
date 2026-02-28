5375c5354b4d66dbf36e5c31ead4224fa028515a
John Allers <john.allers@conductorone.com>
2026-02-28 15:33:38 -0500

    cleanup

--- pkg/client/urls.go
+++ pkg/client/urls.go
@@ -1,9 +1,9 @@
 package client
 
 const (
-	BaseURL    = "https://rest.ripplingapis.com"
-	UsersURL   = BaseURL + "/users"
-	TeamsURL   = BaseURL + "/teams"
-	WorkersURL        = BaseURL + "/workers"
-	WorkLocationsURL  = BaseURL + "/work-locations"
+	BaseURL          = "https://rest.ripplingapis.com"
+	UsersURL         = BaseURL + "/users"
+	TeamsURL         = BaseURL + "/teams"
+	WorkersURL       = BaseURL + "/workers"
+	WorkLocationsURL = BaseURL + "/work-locations"
 )
